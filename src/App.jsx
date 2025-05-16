1b96b855c2a0b8bbfeae363f6a1973043a73926c
saurabh garkoti <saurabhgarkotiii@gmail.com>
2025-05-16 17:00:38 +0530

    gta 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
 import { useGSAP } from '@gsap/react'
+import gsap from "gsap";
 
 function App() {
+
+  useGSAP(()=>{
+    const tl = gsap.timeline();
+
+    tl.to(".vi-mask-group", {
+      rotate: 10,
+      duration: 2,
+      ease: "power4.easeInOut",
+      transformOrigin: "50% 50%",
+
+    })
+    .to(".vi-mask-group",{
+      scale: 10,
+      duration: 2,
+      delay: -1.8,
+      ease: "Expo.easeInOut",
+      transformOrigin: "50% 50%",
+      opacity: 0,
+      onUpdate: function (){
+        if(this.progress()>=0.9) {
+          document.querySelector(".svg").remove();
+          setShowContent(true);
+          this.kill();
+        }
+      }
+    })
+  });
   return (
    <>
     <div className="svg flex items-center justify-center fixed top-0 left-0 z-[100] w-full h-screen overflow-hidden bg-[#000]">
@@ -33,6 +61,7 @@ function App() {
           />
         </svg>
       </div>
+      
    </>
   )
 }
